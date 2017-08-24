ad4f7268b23322cfac66ad9b29026306a08b632d
张一鹏 <yipeng.zhang@zymobi.com>
2017-08-24 16:01:18 +0800

    Zhengli

--- main_server.js
+++ main_server.js
@@ -1,40 +1,51 @@
 var http = require('http');
+var URL = require('url');
 
 http.createServer(function(request, response){
-    var result = {status:"error"};
     console.log("receive a request!!!");
-    switch(request.url){
-    case "/dytt":
-        result.status = "ok";
-        var fetch = require("./spider/spider_dytt");
-        fetch.startRequest("",function(resultArray){
-            result.data = resultArray;
-            responseEnd(response,result);
-        })
-    break;
-    case "/appcanbbs":
-        result.status = "ok";
-        var fetch = require("./spider/spider_appcanbbs");
-        fetch.startRequest("",function(resultArray){
-            result.data = resultArray;
-            responseEnd(response,result);
-        })
-        break;
-    case "/xiaoshuo":
-        result.status = "ok";
-        var fetch = require("./spider/spider_dingdianxiaoshuo");
-        fetch.startRequest("",function(resultArray){
-            result.data = resultArray;
-            responseEnd(response,result);
-        })
-        break;
-    default:
-        result.status = "fail";
-        result.data = "unknown request!!!"
-        responseEnd(response,result);
-    break;
-    }
+    handleRequest2(request, response);
 }).listen(9999);
+
+function handleRequest2(request, response){
+    var result = {status:"error"};
+    var url = URL.parse(request.url, true);
+    responseEnd(response,result);
+}
+
+function handleRequest(request, response){
+    var result = {status:"error"};
+    switch(request.url){
+        case "/dytt":
+            result.status = "ok";
+            var fetch = require("./spider/spider_dytt");
+            fetch.startRequest("",function(resultArray){
+                result.data = resultArray;
+                responseEnd(response,result);
+            })
+            break;
+        case "/appcanbbs":
+            result.status = "ok";
+            var fetch = require("./spider/spider_appcanbbs");
+            fetch.startRequest("",function(resultArray){
+                result.data = resultArray;
+                responseEnd(response,result);
+            })
+            break;
+        case "/xiaoshuo":
+            result.status = "ok";
+            var fetch = require("./spider/spider_dingdianxiaoshuo");
+            fetch.startRequest("",function(resultArray){
+                result.data = resultArray;
+                responseEnd(response,result);
+            })
+            break;
+        default:
+            result.status = "fail";
+            result.data = "unknown request!!!"
+            responseEnd(response,result);
+            break;
+    }
+}
 
 function responseEnd(response,result){
     response.end(JSON.stringify(result));
